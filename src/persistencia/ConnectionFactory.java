214f1ca9e3f19eb03d9882e5b00e310d15bc9872
a1320548 <a1320548@L2-08.HTO.IFSP.EDU.BR>
2015-06-11 09:39:21 -0300

    Adicionado JDBCDAO do jogador. e conection factory

---
+++ src/persistencia/ConnectionFactory.java
@@ -0,0 +1,29 @@
+package persistencia;
+
+import java.sql.Connection;  
+import java.sql.DriverManager;  
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Fabio 
+ */
+public class ConnectionFactory {
+    
+    private static Connection connection;
+    
+    private ConnectionFactory(){
+        
+    }
+    
+    public static Connection getConexao(){
+            if(connection == null){
+                    try{
+                        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chat", "root","");
+                    } catch (Exception e){
+                        e.printStackTrace();
+                        JOptionPane.showMessageDialog(null,"Erro de conexao ao banco de dados!");
+                    }
+            }
+            return connection;
+    }
+}
